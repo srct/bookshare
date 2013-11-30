f8520771473c0eeca7f3281ee731193dac25e23f
Jean Michel Rouly <michel.rouly@gmail.com>
2013-11-30 15:16:42 -0500

    Removed pagination temporarily.

--- bookshare/website/views.py
+++ bookshare/website/views.py
@@ -7,20 +7,20 @@ from django.core.paginator import Paginator, InvalidPage, EmptyPage
 # home page
 def index(request):
 
-    # front page of the site shows the 12 most recent listings
-    products = Books.objects.all().order_by("-uploaded")
-    paginator = Paginator(products, 12)
-
-    # can we get a first page please?
-    try:
-        page = int(request.GET.get("page", '1'))
-    except ValueError: page = 1
-
-    # how many pages do we have?
-    try:
-        products = paginator.page(page)
-    except (InvalidPage, EmptyPage):
-        blogs = paginator.page(paginator.num_pages)
+#    # front page of the site shows the 12 most recent listings
+#    products = Book.objects.all().order_by("-uploaded")
+#    paginator = Paginator(products, 12)
+#
+#    # can we get a first page please?
+#    try:
+#        page = int(request.GET.get("page", '1'))
+#    except ValueError: page = 1
+#
+#    # how many pages do we have?
+#    try:
+#        products = paginator.page(page)
+#    except (InvalidPage, EmptyPage):
+#        blogs = paginator.page(paginator.num_pages)
     
     # need to figure out what needs to be displayed/accessed
     return render_to_response('index.html', {

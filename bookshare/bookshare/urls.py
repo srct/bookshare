2d9ce94dc2a3cd65b4b2fd5d87bb728474be1065
Jean Michel Rouly <michel.rouly@gmail.com>
2013-11-30 14:45:01 -0500

    Updated name of module.

--- bookshare/bookshare/urls.py
+++ bookshare/bookshare/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
+from django.contrib import auth
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('website.views',
     # Examples:
     # url(r'^$', 'bookshare.views.home', name='home'),
     # url(r'^bookshare/', include('bookshare.foo.urls')),

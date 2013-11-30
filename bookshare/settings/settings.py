4d870bff18751c8e05e2a97c3ad65e79d35a3ffe
Jean Michel Rouly <michel.rouly@gmail.com>
2013-11-30 14:54:06 -0500

    Renamed bookshare->settings.

--- bookshare/settings/settings.py
+++ bookshare/settings/settings.py
@@ -102,10 +102,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'bookshare.urls'
+ROOT_URLCONF = 'settings.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'bookshare.wsgi.application'
+WSGI_APPLICATION = 'settings.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".

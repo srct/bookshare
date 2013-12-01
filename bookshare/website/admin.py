c7520627d0d1d2bd872001305f2dda1d5c794aa1
Jean Michel Rouly <michel.rouly@gmail.com>
2013-11-30 20:58:32 -0500

    Commited for kunal.

--- bookshare/website/admin.py
+++ bookshare/website/admin.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from django.forms import CheckboxSelectMultiple
-from website.models import *
-from website.forms import *
+from website.models import Listing, Seller
 
 admin.site.register(Listing)
 admin.site.register(Seller)

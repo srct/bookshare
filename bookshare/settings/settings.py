93d73c52fee45c2da189e8d569512d2bcf36bc0d
Jean Michel Rouly <michel.rouly@gmail.com>
2013-11-30 19:50:33 -0500

    Installed website app. Woops.

--- bookshare/settings/settings.py
+++ bookshare/settings/settings.py
@@ -122,6 +122,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website',
+    'south',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:

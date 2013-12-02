1e81a1e56205972abba49b6f0ee83fd8f422497e
Jean Michel Rouly <michel.rouly@gmail.com>
2013-12-01 23:34:29 -0500

    Fixed NULLable fields

--- bookshare/website/models.py
+++ bookshare/website/models.py
@@ -7,11 +7,11 @@ class Listing( models.Model ):
     title = models.CharField(max_length = 200)
     author = models.CharField(max_length = 200)
     ISBN = models.CharField(max_length = 15)
-    year = models.IntegerField(blank=True)
+    year = models.IntegerField(null=True,blank=True)
     edition = models.CharField(blank=True,max_length = 30)
 
     date_created = models.DateField()
-    date_sold = models.DateField(blank=True)
+    date_sold = models.DateField(null=True,blank=True)
     condition = models.TextField()
     description = models.TextField(blank=True)
     price = models.IntegerField()
@@ -21,7 +21,7 @@ class Listing( models.Model ):
 
     slug = models.SlugField(max_length = 50)
 
-    finalPrice = models.IntegerField()
+    finalPrice = models.IntegerField(null=True,blank=True)
 
     # object call
     def __unicode__(self):
@@ -37,6 +37,7 @@ class Seller( models.Model ):
     name = models.CharField(max_length = 200, primary_key=True)
     username = models.CharField(max_length = 200)
     email = models.CharField(max_length = 200)
+    rating = models.IntegerField(null=True,blank=True)
 
     # object call
     def __unicode__(self):

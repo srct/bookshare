2d93d6300fd555d30287763dd1cfe775129be740
Jean Michel Rouly <michel.rouly@gmail.com>
2013-12-01 19:26:48 -0500

    Installed comments.

--- bookshare/settings/settings.py
+++ bookshare/settings/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.comments',
     'website',
     'south',
     # Uncomment the next line to enable the admin:

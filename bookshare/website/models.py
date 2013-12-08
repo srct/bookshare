23f30748e2f0174163ccf49b79e48f20d0c0883e
Jean Michel Rouly <michel.rouly@gmail.com>
2013-12-08 18:12:04 -0500

    Book condition uses choices array.

--- bookshare/website/models.py
+++ bookshare/website/models.py
@@ -1,10 +1,28 @@
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.db import models
+from django.utils import timezone
 from datetime import datetime
 
 # Create your models here.
 class Listing( models.Model ):
+
+    NEW = 'New'
+    LIKE_NEW = 'Like New'
+    VERY_GOOD = 'Very Good'
+    GOOD = 'Good'
+    ACCEPTABLE = 'Acceptable'
+    UNACCEPTABLE = 'Unacceptable'
+
+    BOOK_CONDITION_CHOICES = (
+        (NEW, 'New'),
+        (LIKE_NEW, 'Like New'),
+        (VERY_GOOD, 'Very Good'),
+        (GOOD, 'Good'),
+        (ACCEPTABLE, 'Acceptable'),
+        (UNACCEPTABLE, 'Unacceptable'),
+    )
+
     seller = models.ForeignKey('Seller')
 
     title = models.CharField(max_length = 200)
@@ -13,9 +31,11 @@ class Listing( models.Model ):
     year = models.IntegerField(null=True,blank=True)
     edition = models.CharField(blank=True,max_length = 30)
 
-    date_created = models.DateTimeField(default=datetime.now())
+    date_created = models.DateTimeField(default=timezone.now())
     date_sold = models.DateTimeField(null=True,blank=True)
-    book_condition = models.TextField()
+    book_condition = models.CharField(choices=BOOK_CONDITION_CHOICES,
+                                      max_length=20,
+                                      default=GOOD)
     description = models.TextField(blank=True)
     price = models.IntegerField()
     photo = models.ImageField(max_length = 1000,upload_to='listing_photos')
@@ -31,7 +51,10 @@ class Listing( models.Model ):
 
     def get_absolute_url(self):
         from django.core.urlresolvers import reverse
-        return reverse('listing', args=[self.seller.username, str(self.id)])
+        return reverse('listing', args=[self.seller.user.username, str(self.id)])
+
+    class Meta:
+        unique_together = (("ISBN", "seller"),)
 
 
 class Seller( models.Model ):

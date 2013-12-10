09db6f1a944a08ca576306b0521939aac886d4cf
Jean Michel Rouly <michel.rouly@gmail.com>
2013-12-09 22:00:05 -0500

    Updated to deployment settings for gunicorn.

--- bookshare/settings/settings.py
+++ bookshare/settings/settings.py
@@ -2,8 +2,7 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-DEVELOPMENT = False
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -25,7 +24,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -56,15 +55,9 @@ MEDIAFILES_DIRS = (
 )
 
 STATIC_URL = '/static/'
-if DEVELOPMENT:
-    STATIC_ROOT = ''
-    STATICFILES_DIRS = (
-        (os.path.join(BASE_DIR, 'static/')),
-    )
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-    STATICFILES_DIRS = (
-    )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = (
+)
 
 # List of finder classes that know how to find static files in
 # various locations.

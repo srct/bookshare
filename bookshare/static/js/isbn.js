e1ab9f801642a2032997ef3c0323f343fcde05fc
Jean Michel Rouly <michel.rouly@gmail.com>
2013-12-13 21:44:00 -0500

    Added ISBN-13 validation, some documentation, and cleaned up the order of validation steps.

--- bookshare/static/js/isbn.js
+++ bookshare/static/js/isbn.js
@@ -1,20 +1,33 @@
 $(function() {
+
+  // add input handler
   $('#id_ISBN').on('input', function(e) {
     e.preventDefault();
 
+    // if the section is already hidden, show it
     if( ! $('#section-collapse').hasClass('in') ){
       $('#section-collapse').collapse('show');
     }
 
+    // grab and validate the ISBN
     var isbn = $('#id_ISBN').val();
     var is_valid = isValidISBN( isbn );
 
-    if( ! is_valid ){
+    // Style the form appropriately.
+    if( ! is_valid ) {
+      $('#form-group-ISBN').addClass('has-error');
+      $('#form-group-ISBN').removeClass('has-success');
+    } else {
+      $('#form-group-ISBN').removeClass('has-error');
+      $('#form-group-ISBN').addClass('has-success');
+    }
+
+    if( ! is_valid ){ // If invalid, clear the form.
       $('#id_title').val( '' );
       $('#id_author').val( '' );
       $('#id_year').val( '' );
       $('#id_edition').val( '' );
-    } else {
+    } else { // Otherwise, poll worldcat for data.
 
       var url = "http://xisbn.worldcat.org/webservices/xid/isbn/" + isbn + "?method=getMetadata&format=json&fl=title,author,year,ed"
 
@@ -25,43 +38,31 @@ $(function() {
 
     }
 
-    if( ! is_valid ) {
-      $('#form-group-ISBN').addClass('has-error');
-      $('#form-group-ISBN').removeClass('has-success');
-    } else {
-      $('#form-group-ISBN').removeClass('has-error');
-      $('#form-group-ISBN').addClass('has-success');
-    }
-
   });
 });
 
 
+// This function takes a JSON response object and grabs the fields from it,
+// spitting them into the form fields.
 function process_ISBN_json( json ) {
-
-  var title = "No results found.";
-  var author = "No results found.";
-  var year = "No results found.";
-  var edition = "No results found.";
-
   // There should only ever be a single element, if any.
-  if( json.list[0] ){
+  if( json.stat == "ok" ){
     title = json.list[0].title;
     author = json.list[0].author;
     year = json.list[0].year;
     edition = json.list[0].ed;
-    
-  }
 
-  $('#id_title').val( title );
-  $('#id_author').val( author );
-  $('#id_year').val( year );
-  $('#id_edition').val( edition );
+    $('#id_title').val( title );
+    $('#id_author').val( author );
+    $('#id_year').val( year );
+    $('#id_edition').val( edition );
+  }
 }
 
 
 function isValidISBN (isbn) { 
   isbn = isbn.replace(/[^\dX]/gi, ''); 
+
   if(isbn.length != 10 && isbn.length != 13){
     return false;
   }
@@ -79,8 +80,16 @@ function isValidISBN (isbn) {
   }
   
   if(isbn.length == 13){
-    var chars = isbn.split('');
+    var check, i;
 
-    return false;
+    check = 0;
+    for( i = 0; i < 13; i += 2 ){
+      check += parseInt(isbn.charAt(i));
+    }
+    for( i = 1; i < 12; i += 2 ){
+      check += 3 * parseInt(isbn.charAt(i));
+    }
+
+    return check % 10 === 0;
   }
 }

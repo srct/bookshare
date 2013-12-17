8d8b15295f1448cfad583d837a914db3997b09e6
Jean Michel Rouly <michel.rouly@gmail.com>
2013-12-17 17:30:27 -0500

    Only allowing ISBN lookouts for now.

--- bookshare/lookouts/models.py
+++ bookshare/lookouts/models.py
@@ -8,16 +8,17 @@ class Lookout( models.Model ):
     owner = models.ForeignKey(Seller)
     date_created = models.DateTimeField(default=timezone.now())
 
-    ISBN = models.CharField(max_length = 20, blank=True)
-    title = models.CharField(max_length = 200, blank=True)
+    ISBN = models.CharField(max_length = 20)
+    # place other possible fields here, ISBN only for right now.
 
     def __unicode__(self):
         return '<Lookout: %s>' % self.owner.user.username
 
     def get_listings(self):
         isbn_listings = models.Q( ISBN=self.ISBN, active=True )
-        title_listings = models.Q( title=self.title, active=True )
-        return Listing.objects.filter( isbn_listings | title_listings )
+        return Listing.objects.filter( isbn_listings )
+#        title_listings = models.Q( title=self.title, active=True )
+#        return Listing.objects.filter( isbn_listings | title_listings )
 
     class Meta:
-        ordering = ['ISBN', 'title']
+        ordering = ['ISBN']

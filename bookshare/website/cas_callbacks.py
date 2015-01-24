5fd6e84ec4ad70c45c5b1e3abb0fcea991926f3c
Daniel W Bond <dbond2@gmu.edu>
2015-01-24 12:13:43 -0500

    added initial callbacks file

---
+++ bookshare/website/cas_callbacks.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.conf import settings
+
+def create_user(tree):
+
+    username = tree[0][0].text
+    user, user_created = User.objects.get_or_create(username=username)
+
+    if user_created:
+        user.email = "%s@%s" % (username, settings.ORGANIZATION_EMAIL_DOMAIN)
+        user.save()
+        new_student = Student.objects.create()
+        new_student.user = user        
+
+        print("Created user %s!" % username)

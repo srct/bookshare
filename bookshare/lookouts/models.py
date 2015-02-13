cb436c499cc75ac02cf47c2117d89ac88c9a9af4
Daniel W Bond <dbond2@gmu.edu>
2015-02-13 14:01:14 -0500

    rearranged model fields

--- bookshare/lookouts/models.py
+++ bookshare/lookouts/models.py
@@ -3,29 +3,28 @@ from trades.models import Listing
 from core.models import Course
 from django.conf import settings
 from model_utils.models import TimeStampedModel
-from autoslug import AutoSlugField
 from randomslugfield import RandomSlugField
 
 class Lookout(TimeStampedModel):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL)
     isbn = models.CharField(max_length = 20)
-    #course = models.ForeignKey('Course')
+    #course = models.ForeignKey(Course)
     # place other possible fields here, ISBN and Course only for right now.
 
-    isbnslug = AutoSlugField(populate_from='isbn')
-    randomslug = RandomSlugField(length=6, exclude_upper=True)
-
-    # needs get_absolute_url
-
-    # this should change
-    def __unicode__(self):
-        return '%s %s' % (self.owner.username, self.isbn)
-
+    slug = RandomSlugField(length=6, exclude_upper=True)
     def get_listings(self):
         isbn_listings = models.Q( isbn=self.isbn, active=True )
         return Listing.objects.filter( isbn_listings )
 #        title_listings = models.Q( title=self.title, active=True )
 #        return Listing.objects.filter( isbn_listings | title_listings )
+
+    # needs get_absolute_url
+    def get_absolute_url(self):
+        return reverse('detail_lookout', kwards={'slug':self.slug})
+
+    # this should change
+    def __unicode__(self):
+        return '%s %s' % (self.owner.username, self.isbn)
 
     class Meta:
         ordering = ['isbn']

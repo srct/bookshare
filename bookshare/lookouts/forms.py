d5764f6b28912fd2139db2fc34b7c423af9648c4
Daniel W Bond <dbond2@gmu.edu>
2015-02-13 14:00:24 -0500

    modified lookout creation form

--- bookshare/lookouts/forms.py
+++ bookshare/lookouts/forms.py
@@ -1,33 +1,28 @@
 from django import forms
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout, Fieldset, ButtonHolder, HTML, Field
-#from crispy_forms.bootstrap import PrependedText
+from crispy_forms.layout import Submit, Layout, Fieldset, HTML, Field
+from crispy_forms.bootstrap import FormActions
 
 from lookouts.models import Lookout
 
 class LookoutForm( forms.ModelForm ):
 
-    def __init__(self, *args, **kwargs):
-
-        self.helper = FormHelper()
-        self.helper.form_method = 'post'
-        self.helper.form_class = 'form-horizontal'
-        self.helper.layout = Layout(
-            Fieldset("Your Layout",
-                'isbn',
-            ),
-        )
-
-        self.helper.add_input(Submit('submit', 'Submit'))
-
-        super(LookoutForm, self).__init__(*args, **kwargs)
+    helper = FormHelper()
+    helper.form_method = 'POST'
+    helper.form_class = 'form-horizontal'
+    helper.label_class='col-sm-2'
+    helper.field_class='col-sm-6'
+    
+    helper.layout = Layout(
+        Fieldset("Your Lookout",
+            Field('isbn', title="ISBN"),
+            # fancy javascript autocomplete eventually
+            #'course',
+            HTML("""<hr/ >"""),
+            FormActions(Submit('submit', 'Submit', css_class='btn-primary'))
+        ),
+    )
 
     class Meta:
         model = Lookout
-
-class DeleteLookoutForm( forms.Form ):
-    lookout_id = forms.IntegerField(
-        required = True,
-        widget=forms.HiddenInput()
-    )

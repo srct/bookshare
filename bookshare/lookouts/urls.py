c93746c59710ca498f27f9d3e16731b012f5e645
Daniel W Bond <dbond2@gmu.edu>
2015-02-13 14:02:03 -0500

    rewrite of lookout handling

--- bookshare/lookouts/urls.py
+++ bookshare/lookouts/urls.py
@@ -1,22 +1,28 @@
 from django.conf.urls import patterns, include, url
 
-from lookouts.views import CreateLookout, DeleteLookout
+from lookouts.views import DetailLookout, CreateLookout, DeleteLookout
 from lookouts.models import Lookout
 
 urlpatterns = patterns('',
 
-    url(r'^(?P<randomslug>[\w-]+)/(?P<isbnslug>[\w-]+)/$',
+    url(r'^new/$',
         CreateLookout.as_view(
-        model=Lookout,
-        context_object_name='lookout',
-        template_name='create_lookout.html'),
+            model=Lookout,
+            template_name='create_lookout.html'),
     name='create_lookout'),
 
-    url(r'^(?P<randomslug>[\w-]+)/(?P<isbnslug>[\w-]+)/delete/$',
+    url(r'^(?P<slug>[\w-]+)/$',
+        DetailLookout.as_view(
+            model=Lookout,
+            context_object_name='lookout',
+            template_name='detail_lookout.html'),
+    name='detail_lookout'),
+
+    url(r'^(?P<slug>[\w-]+)/delete/$',
         DeleteLookout.as_view(
-        model=Lookout,
-        context_object_name='lookout',
-        template_name='delete_lookout.html'),
+            model=Lookout,
+            context_object_name='lookout',
+            template_name='delete_lookout.html'),
     name='delete_lookout'),
         
 )
--- bookshare/lookouts/views.py
+++ bookshare/lookouts/views.py
@@ -1,7 +1,7 @@
 from lookouts.models import Lookout
 from lookouts.forms import LookoutForm
 
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from braces.views import LoginRequiredMixin
 
 ### VIEWS ###
@@ -9,7 +9,13 @@ class CreateLookout(LoginRequiredMixin, CreateView):
     # can only be viewed by the user who created the lookout!...
     model = Lookout
     form_class = LookoutForm
-    success_url = '/'
+#    success_url = '/'
+# should redirect to get_absolute_url
+    login_url = '/'
+
+class DetailLookout(LoginRequiredMixin, DetailView):
+    model = Lookout
+    context_object_name = 'lookout'
     login_url = '/'
 
 # remember, see all the lookouts on the homepage

9ade2ffe6d22b29d30a5e4dcba10ad2b1abd84d7
Daniel W Bond <dbond2@gmu.edu>
2015-03-12 15:16:52 -0400

    added links to student profile page

--- bookshare/core/views.py
+++ bookshare/core/views.py
@@ -76,11 +76,33 @@ class DetailStudent(LoginRequiredMixin, DetailView):
     model = Student
 
     def get_context_data(self, **kwargs):
+
+        def total_sales(listings):
+            sales = 0
+            for listing in listings:
+                if listing.sold:
+                    sales = sales + 1
+            return sales
+
+        def total_proceeds(listings):
+            proceeds = 0
+            for listing in listings:
+                if listing.sold:
+                    proceeds = proceeds + listing.finalPrice
+            return proceeds
+
+        student_listings = Listing.objects.filter(seller=self.get_object().pk)
+
         context = super(DetailStudent, self).get_context_data(**kwargs)
+
         #context['listings'] = Listing.objects.filter(seller='2')
-        context['listings'] = Listing.objects.filter(seller=self.get_object().pk)
+        context['listings'] = student_listings
         context['me'] = self.get_object().pk
         context['lookouts'] = Lookout.objects.filter(owner=self.get_object().user)
+
+        context['proceeds'] = total_proceeds(student_listings)
+        context['sales'] = total_sales(student_listings)
+
         return context
 
     login_url = '/'

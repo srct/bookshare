9ab49412c8a92af03c091136489e20fc3e5bae41
Daniel W Bond <dbond2@gmu.edu>
2015-04-04 07:06:32 -0400

    trying to figure out peoplefinder incorporation

--- bookshare/core/cas_callbacks.py
+++ bookshare/core/cas_callbacks.py
@@ -1,13 +1,37 @@
 from django.contrib.auth.models import User
 from django.conf import settings
 
+from .models import Student
+
+import requests
+
+def pfinfo(u_name):
+    pf_url = "http://peoplefinder.b1.akshaykarthik.com/"
+    url = str(pf_url) + "basic/all/" + str(u_name)
+    try:
+        metadata = requests.get(url)
+        metadata.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print e
+    else:
+        pf_json = metadata.json()
+        name = pf_json['results'][0]['name']
+        return name.split(',')
+
 def create_user(tree):
 
     username = tree[0][0].text
+    print username
     user, user_created = User.objects.get_or_create(username=username)
 
     if user_created:
         user.email = "%s@%s" % (username, settings.ORGANIZATION_EMAIL_DOMAIN)
+#        print "hello"
+#        name_list = pfinfo(str(username))
+#        print "world"
+#        user.first_name = name_list[1].rstrip()
+#        print "something"
+#        user.last_name = name_list[0]
         user.save()
         new_student = Student.objects.create(user=user)
         new_student.save()

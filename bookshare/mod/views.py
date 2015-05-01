54998beded78ed02406ed982c0a4acc7852564d8
Daniel W Bond <dbond2@gmu.edu>
2015-05-01 17:47:52 -0400

    defined mod querysets

--- bookshare/mod/views.py
+++ bookshare/mod/views.py
@@ -1,14 +1,17 @@
 # core django imports
 from django.views.generic import TemplateView, ListView
-from django.db.models import Sum
+from django.db.models import Count
 # third party imports
 from braces.views import LoginRequiredMixin, SuperuserRequiredMixin
 # imports from your apps
 from trades.models import Listing
+from core.models import Student
 
 
 class ModLandingView(LoginRequiredMixin, SuperuserRequiredMixin, TemplateView):
     template_name = 'mod.html'
+
+    login_url = 'login'
 
 class FlagModView(LoginRequiredMixin, SuperuserRequiredMixin, ListView):
     queryset = Listing.objects.annotate(num_flags=Count('flag')).order_by('-num_flags')[:20]
@@ -17,8 +20,8 @@ class FlagModView(LoginRequiredMixin, SuperuserRequiredMixin, ListView):
     login_url = 'login'
 
 class ListingNumModView(LoginRequiredMixin, SuperuserRequiredMixin, ListView):
-    queryset = Listing.objects.all()[:20]
-    context_object_name = 'users'
+    queryset = Student.objects.annotate(num_books=Count('listing')).order_by('-num_books')[:20]
+    context_object_name = 'students'
     template_name = 'listing_num_mod.html'
     login_url = 'login'
 
@@ -26,5 +29,10 @@ class UserEmailRatioModView(LoginRequiredMixin, SuperuserRequiredMixin, Template
     template_name = 'email_ratio_mod.html'
 
     def get_context_data(self, **kwargs):
-        context = super(UserEmailRatioView, self).get_context_data(**kwargs)
+        context = super(UserEmailRatioModView, self).get_context_data(**kwargs)
+
+        students_by_emails = Student.objects.order_by('-emails_sent')[:20]
+        students_listings = students_by_emails.annotate(num_books=Count('listing'))
+
+        context['email_happy_students'] = students_listings
         return context

267f3fceddf277b19bf12c04e3d4235d00cd626a
Mark D Stenglein <mstengle@gmu.edu>
2016-04-04 10:31:39 -0400

    Split student and user creation and added exception handling for peoplefinder in CAS callback.

--- bookshare/core/cas_callbacks.py
+++ bookshare/core/cas_callbacks.py
@@ -1,4 +1,3 @@
-<<<<<<< b92a75e64037f60a7eef65f6e4d4089620a9400d
 # core django imports
 from django.conf import settings
 from django.contrib.auth.models import User
@@ -77,64 +76,3 @@ def create_user(tree):
         print("Created student object for user %s!" % username)
 
     print("CAS callback completed.")
-
-=======
-# core django imports
-from django.conf import settings
-from django.contrib.auth.models import User
-# third-part imports
-import requests
-# imports from your apps
-from django.conf import settings
-from .models import Student
-
-
-def pfinfo(u_name):
-    pf_url = settings.PF_URL
-    url = str(pf_url) + "basic/all/" + str(u_name)
-    try:
-        metadata = requests.get(url)
-        metadata.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        print e
-    else:
-        pf_json = metadata.json()
-        try:
-            name = pf_json['results'][0]['name']
-            return name.split(',')
-        # if the name is not in peoplefinder, return empty first and last name
-        except IndexError:
-            return ['', '']
-
-
-def create_user(tree):
-    username = tree[0][0].text
-    print username
-    user, user_created = User.objects.get_or_create(username=username)
-
-    if user_created:
-        user.email = "%s@%s" % (username, settings.ORGANIZATION_EMAIL_DOMAIN)
-        print "hello"
-        name_list = pfinfo(str(username))
-        print name_list, "name_list"
-        first_name = name_list[1].lstrip().split(' ')
-        if len(first_name) > 1:
-            no_mi = first_name[:-1]
-            user.first_name = ' '.join(no_mi)
-        else:
-            user.first_name = ' '.join(first_name)
-        last_name = name_list[0]
-        user.last_name = name_list[0]
-
-        print "world"
-        user.save()
-        new_student = Student.objects.create(user=user)
-
-        # save the name off of peoplefinder for later quality assurance purposes
-        new_student.pf_first_name = user.first_name
-        new_student.pf_last_name = user.last_name
-
-        new_student.save()
-
-        print("Created user %s!" % username)
->>>>>>> added support for storing peoplefinder names (helps with moderation)

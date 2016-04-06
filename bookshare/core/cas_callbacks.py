afe5ec5c05f2c7381b64d5554826d8921a50187b
Mark D Stenglein <mstengle@gmu.edu>
2016-04-06 19:58:35 -0400

    Revert "Merge branch 'loginError' into '1.0'"
    This reverts commit d3dd949fdd238abc28459a79ca40a0f0a9955719, reversing
    changes made to dcc97bd431bab144589092362f7cbd52bb39f6eb.

--- bookshare/core/cas_callbacks.py
+++ bookshare/core/cas_callbacks.py
@@ -1,7 +1,6 @@
 # core django imports
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.core.exceptions import ObjectDoesNotExist
 # third-part imports
 import requests
 # imports from your apps
@@ -34,38 +33,20 @@ def create_user(tree):
 
     if user_created:
         user.email = "%s@%s" % (username, settings.ORGANIZATION_EMAIL_DOMAIN)
-        user.set_password('cas_used_instead')
-        print("Added user email and default password.")
+        print "hello"
+        name_list = pfinfo(str(username))
+        print name_list, "name_list"
+        first_name = name_list[1].lstrip().split(' ')
+        if len(first_name) > 1:
+            no_mi = first_name[:-1]
+            user.first_name = ' '.join(no_mi)
+        else:
+            user.first_name = ' '.join(first_name)
+        last_name = name_list[0]
+        user.last_name = name_list[0]
 
-        print "Start peoplefinder parsing"
-        try:
-            name_list = pfinfo(str(username))
-            print name_list, "name_list"
-            first_name = name_list[1].lstrip().split(' ')
-            if len(first_name) > 1:
-                no_mi = first_name[:-1]
-                user.first_name = ' '.join(no_mi)
-            else:
-                user.first_name = ' '.join(first_name)
-            last_name = name_list[0]
-            user.last_name = name_list[0]
-
-        except Exception as e:
-            print("Unhandled peoplefinder exception:", e)
-
+        print "world"
         user.save()
-        print("Created user %s!" % username)
-
-    else:
-        print("User object already exists.")
-
-
-    # Student Creation Section
-    try:
-        Student.objects.get(user=user)
-        print("Student object already exists")
-
-    except ObjectDoesNotExist:
         new_student = Student.objects.create(user=user)
 
         # save the name off of peoplefinder for later quality assurance purposes
@@ -73,7 +54,5 @@ def create_user(tree):
         new_student.pf_last_name = user.last_name
 
         new_student.save()
-        print("Created student object for user %s!" % username)
 
-    print("CAS callback completed.")
-
+        print("Created user %s!" % username)

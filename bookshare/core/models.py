5c90508e9ad27717d42191543972eeeb1afa4a7b
Mark D Stenglein <mstengle@gmu.edu>
2016-04-08 00:03:59 -0400

    Added method in Student model to fetch either full name or username if none present.

--- bookshare/core/models.py
+++ bookshare/core/models.py
@@ -19,6 +19,12 @@ class Student(TimeStampedModel):
 
     emails_sent = models.PositiveIntegerField(default=0)
 
+    def get_full_name(self):
+        if (self.user.get_full_name() == ""):
+            return self.user.username
+        else:
+            return self.user.get_full_name()
+
     def has_nickname(self):
         pf_name = "%s %s" % (self.pf_first_name, self.pf_last_name)
 

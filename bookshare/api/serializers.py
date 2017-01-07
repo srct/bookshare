5eaa23695f17308d6d7bcdf218211d830f92d026
Daniel W Bond <dbond2@gmu.edu>
2017-01-06 22:40:04 -0500

    no need for admin or model files in the api app, but *do* need serializers and urls

---
+++ bookshare/api/serializers.py
@@ -0,0 +1,5 @@
+# standard library imports
+from __future__ import absolute_import, print_function, unicode_literals
+# third party imports
+# imports from your apps
+from trades.models import Listing
---
+++ bookshare/api/urls.py
@@ -0,0 +1,9 @@
+# standard libary imports
+from __future__ import absolute_import, print_function, unicode_literals
+# core django imports
+from django.conf.urls import patterns, include, url
+
+# API v1
+
+urlpatterns = patterns('',
+)

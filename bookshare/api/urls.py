54786b450e9158008e12f7a18e7bff72101b7106
Daniel W Bond <dbond2@gmu.edu>
2017-01-17 14:23:53 -0500

    added that listing view to a DefaultRouter

--- bookshare/api/urls.py
+++ bookshare/api/urls.py
@@ -1,9 +1,15 @@
 # standard libary imports
 from __future__ import absolute_import, print_function, unicode_literals
 # core django imports
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+# third party imports
+from rest_framework.routers import DefaultRouter
+# imports from your apps
+from .views import ListingViewSet
 
-# API v1
 
-urlpatterns = patterns('',
-)
+router = DefaultRouter()
+
+router.register(r'listings', ListingViewSet, 'listing_list')
+
+urlpatterns = router.urls

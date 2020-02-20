e2accd2f217ef6547d1c71d7a2c19680018180a5
Daniel W Bond <dbond2@gmu.edu>
2020-02-20 17:06:33 -0500

    added back default logging for local settings... this still needs to be split apart

--- bookshare/settings/settings.py
+++ bookshare/settings/settings.py
@@ -271,3 +271,31 @@ if not DEBUG:
         # or CRITICAL level to the AdminEmailHandler via mail_admins
         },
     }
+else:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'filters': {
+            'require_debug_false': {
+                '()': 'django.utils.log.RequireDebugFalse'
+            },
+        },
+        'handlers': {
+            'mail_admins': {
+                'level': 'ERROR',
+                'filters': ['require_debug_false'],
+                'class': 'django.utils.log.AdminEmailHandler'
+            },
+            'console':{
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler'
+            },
+        },
+        'loggers': {
+            'django.request': {
+                'handlers': ['mail_admins'],
+                'level': 'ERROR',
+                'propagate': True,
+            },
+        },
+    }

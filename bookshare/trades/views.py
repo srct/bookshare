fe6b657ff602b4c664cc4375f02b1d8539f4a3fe
Daniel W Bond <dbond2@gmu.edu>
2020-02-24 16:39:01 -0500

    remove functions from top of file, and import from newly created utils

--- bookshare/trades/views.py
+++ bookshare/trades/views.py
@@ -13,7 +13,6 @@ from django.template.loader import get_template
 from django.template import Context
 from django.utils.safestring import mark_safe
 # third party imports
-import requests
 from PIL import Image
 from braces.views import LoginRequiredMixin, SuperuserRequiredMixin
 from braces.views import FormValidMessageMixin
@@ -22,77 +21,9 @@ from ratelimit.decorators import ratelimit
 from .models import Listing, Bid, Flag, BidFlag, Rating
 from .forms import ListingForm, BidForm, FlagForm, BidFlagForm,\
                    ExchangeListingForm, UnExchangeListingForm, RatingForm
+from .utils import ISBNMetadata, can_flag, can_flag_bid, flag_slug,\
+                   bid_flag_slug, can_rate
 from core.models import Student
-
-
-# pulls worldcat metadata from ISBNs
-def ISBNMetadata(standardISBN):
-    # passing in numbers starting with 0 throws "SyntaxError: invalid token"
-    url = "http://xisbn.worldcat.org/webservices/xid/isbn/" +\
-          str(standardISBN) +\
-          "?method=getMetadata&format=json&fl=title,year,author,ed"
-
-    # In case the API fails to return, simply return None.
-    try:
-        metadata = requests.get(url, timeout=3)
-    except requests.ConnectionError:
-        return None
-
-    # format into a dictionary
-    dejson = metadata.json()
-    try:
-        metadataDict = dejson.get('list')
-        return metadataDict[0]
-    except:
-        return None
-
-
-# flagging
-# you can only flag a listing once...
-def can_flag(flagger, listing):
-    user_flag_num = Flag.objects.filter(flagger=flagger,
-                                        listing=listing).count()
-    # we're assuming that this isn't going to go over 1
-    if user_flag_num:
-        return False
-    else:
-        return True
-
-
-def can_flag_bid(flagger, bid):
-    user_flag_num = BidFlag.objects.filter(flagger=flagger,
-                                           bid=bid).count()
-    if user_flag_num:
-        return False
-    else:
-        return True
-
-
-# get the listing's slug to pass to the create flag page
-def flag_slug(flagger, listing):
-    if not can_flag(flagger, listing):
-        return Flag.objects.get(flagger=flagger, listing=listing).slug
-    else:
-        return None
-
-
-def bid_flag_slug(flagger, bid):
-    if not can_flag_bid(flagger, bid):
-        return BidFlag.objects.get(flagger=flagger, bid=bid).slug
-    else:
-        return None
-
-
-# rating
-# (basically) duplicated code!!!
-def can_rate(rater, listing):
-    user_rate_num = Rating.objects.filter(rater=rater,
-                                          listing=listing).count()
-    # we're assuming that this isn't going to go over 1
-    if user_rate_num:
-        return False
-    else:
-        return True
 
 
 class ListListings(LoginRequiredMixin, ListView):

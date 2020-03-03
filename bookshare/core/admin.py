9b3f64bf17c58bc8f77b815f864b4f8d6fd08ed9
Daniel W Bond <dbond2@gmu.edu>
2020-03-03 18:50:06 -0500

    additional information for new students on admin page

--- bookshare/core/admin.py
+++ bookshare/core/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 # imports from your apps
 from .models import Student, Course
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'created')
+
+admin.site.register(Student, StudentAdmin)
 admin.site.register(Course)
